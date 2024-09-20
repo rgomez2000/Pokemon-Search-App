c2f1feecad782a332cf15d0868cb82ce06513a17
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-09-19 23:17:50 -0700

    Updated JS
    Updated JS elements to contain more information on the stats,

--- script.js
+++ script.js
@@ -32,17 +32,17 @@ const setPokemonInfo = (data) => {
 
   pokemonName.textContent = `${name[0].toUpperCase() + name.slice(1)}`;
   pokemonId.textContent = `#${id}`;
-  pokemonWeight.textContent = `Weight: ${weight}`;
-  pokemonHeight.textContent = `Height: ${height}`;
+  pokemonWeight.textContent = `Weight (Kg): ${weight}`;
+  pokemonHeight.textContent = `Height (Meters): ${height}`;
 
   spriteContainer.innerHTML = `<img id="sprite" src="${sprites.front_default}" alt="${name}">`;
 
-  hp.textContent = stats[0].base_stat;
-  attack.textContent = stats[1].base_stat;
-  defense.textContent = stats[2].base_stat;
-  specialAttack.textContent = stats[3].base_stat;
-  specialDefense.textContent = stats[4].base_stat;
-  speed.textContent = stats[5].base_stat;
+  hp.textContent = `HP: ${stats[0].base_stat}`;
+  attack.textContent = `Attack: ${stats[1].base_stat}`;
+  defense.textContent = `Defense: ${stats[2].base_stat}`;
+  specialAttack.textContent = `Sp.Attack: ${stats[3].base_stat}`;
+  specialDefense.textContent = `Sp.Def ${stats[4].base_stat}`;
+  speed.textContent = `Speed: ${stats[5].base_stat}`;
 
   pokemonTypes.innerHTML = types.map(obj => `<span>${obj.type.name[0].toUpperCase() + obj.type.name.slice(1)}</span>`).join(" ");
 };
